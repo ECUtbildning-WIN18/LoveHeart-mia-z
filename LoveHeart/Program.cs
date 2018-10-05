ff88bd617770a8e5c2ccd15a66b9a3186fd87882
unknown <iperish.x@gmail.com>
2018-10-05 04:27:43 +0200

    initial commit and file creation

---
+++ LoveHeart/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveHeart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo whaddup stop reading my shit i havent committed anything smfh.")
+        }
+    }
+}
