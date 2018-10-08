ce873df8b2cddb9bb3dbe0de25687de639fa1d0a
unknown <iperish.x@gmail.com>
2018-10-08 07:08:57 +0200

    preliminary changes for pull on other device v2

--- LoveHeart/Program.cs
+++ LoveHeart/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace LoveHeart
 {
     class Program
     {
+        public List<User> users;
+
         static void Main(string[] args)
         {
+
+
             Console.SetWindowSize(81, 26);
             MenuHandler mh = new MenuHandler();
             mh.Run();
